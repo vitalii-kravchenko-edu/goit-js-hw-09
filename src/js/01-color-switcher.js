290f597894c7be86360ee372fdcf3aeb0e7ce71c
Vitalii Kravchenko <opabbk@gmail.com>
2023-11-13 21:00:51 +0200

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const refs = {
+  body: document.querySelector('body'),
+  btnStart: document.querySelector('[data-start]'),
+  btnStop: document.querySelector('[data-stop]'),
+};
 
+let timerId = null;
+
+refs.btnStart.addEventListener('click', () => {
+  refs.btnStart.disabled = true;
+  refs.btnStop.disabled = false;
+  changeBodyColor();
+  timerId = setInterval(changeBodyColor, 1000);
+});
+
+refs.btnStop.addEventListener('click', () => {
+  refs.btnStart.disabled = false;
+  refs.btnStop.disabled = true;
+  clearInterval(timerId);
+});
+
+function changeBodyColor() {
+  refs.body.style.background = `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}

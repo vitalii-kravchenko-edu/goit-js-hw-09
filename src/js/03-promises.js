5b923235d7e2ef88e5629547640ea3fd59fe0662
Vitalii Kravchenko <opabbk@gmail.com>
2023-11-19 12:22:01 +0200

    task3-fix2

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -4,6 +4,8 @@ const form = document.querySelector('.form');
 let delay = 0;
 let step = 0;
 let amount = 0;
+let currentDelay = 0;
+let i = 0;
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
@@ -12,7 +14,7 @@ form.addEventListener('submit', (e) => {
   amount = Number(form.elements.amount.value);
 
   for (i = 1; i <= amount; i += 1) {
-    const currentDelay = delay + (i - 1) * step;
+    currentDelay = delay + (i - 1) * step;
 
     createPromise(i, currentDelay)
       .then(({ position, delay }) => {
